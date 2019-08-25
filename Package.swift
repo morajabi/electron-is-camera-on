1331e9fd621676ac3284c1750e80cf0e9c5a076f
Mo Rajabifard <mo.rajbi@gmail.com>
2019-08-25 18:19:52 +0430

    Downgrade swift and electronify

--- Package.swift
+++ Package.swift
@@ -1,11 +1,8 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
 	name: "main",
-	platforms: [
-		.macOS(.v10_11)
-	],
 	dependencies: [
 		.package(url: "https://github.com/sindresorhus/is-camera-on", from: "2.0.1")
 	],
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-'use strict';
-const macosVersion = require('macos-version');
-const path = require('path');
-const EventEmitter = require('events');
-const execa = require('execa');
+'use strict'
+const macosVersion = require('macos-version')
+const path = require('path')
+const execa = require('execa')
 
-const electronUtil = require('electron-util/node');
+const electronUtil = require('electron-util/node')
 
-const binary = path.join(electronUtil.fixPathForAsarUnpack(__dirname), 'main');
+const binary = path.join(electronUtil.fixPathForAsarUnpack(__dirname), 'main')
 
 module.exports = async () => {
-	macosVersion.assertGreaterThanOrEqualTo('10.11');
-	return (await execa.stdout(bin)) === 'true';
-};
+	macosVersion.assertGreaterThanOrEqualTo('10.11')
+	return (await execa.stdout(binary)) === 'true'
+}
